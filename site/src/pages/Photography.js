dd85d3ae64274ff91375c281d96d04fd5e078164
CamH04 <cameronahaynes04@gmail.com>
2024-09-15 15:08:03 +0100

    added photo grid with test photos

--- site/src/pages/Photography.js
+++ site/src/pages/Photography.js
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
 
+import img1 from './img/grid/test1.jpg';
+import img2 from './img/grid/test2.jpg';
+import img3 from './img/grid/test3.jpg';
+
 export default function Photography(){
     useEffect(() => {
         Aos.init();
@@ -11,9 +15,27 @@ export default function Photography(){
 
 
     return(
-        <section>
-            <div>
-                <h1>its working</h1>
+        <section class="photo-grid" data-aos="zoom-in" data-aos-duration="1000">
+            <div class="header">
+                <h1>Some Photos I've Taken</h1>
+                <p>"volare sicut tyto noctua , to fly like a nightingale"</p>
+            </div>
+            <div class="row" data-aos="fade-up" data-aos-duration="2000">
+                <div class="column" data-aos="zoom-in-right" data-aos-duration="2000">
+                    <img src={img1} alt="cphoto"></img>
+                    <img src={img2} alt="cphoto"></img>
+                    <img src={img3} alt="cphoto"></img>
+                </div> 
+                <div class="column" data-aos="zoom-in" data-aos-duration="2000">
+                    <img src={img1} alt="cphoto"></img>
+                    <img src={img2} alt="cphoto"></img>
+                    <img src={img3} alt="cphoto"></img>
+                </div> 
+                <div class="column"data-aos="zoom-in-left" data-aos-duration="2000">
+                    <img src={img1} alt="cphoto"></img>
+                    <img src={img2} alt="cphoto"></img>
+                    <img src={img3} alt="cphoto"></img>
+                </div> 
             </div>
         </section>
     );
